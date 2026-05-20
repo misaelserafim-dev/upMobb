83ff6208f409c5ced537d8e440a100b33c1cf041
Misael Serafim <misael.serafim95@gmail.com>
2026-05-20 16:10:27 -0300

    list js, html

--- scripts/contracts/list.js
+++ scripts/contracts/list.js
@@ -3,14 +3,23 @@ import { renderContracts, renderLoading, renderError } from "./contracts.list.js
 
 let contracts = [];
 
+// carrega a lista
 async function loadContracts() {
     try {
         renderLoading(content);
         contracts = await getContracts();
-        updateList();
     } catch (error) {
         renderError(content);
     }
 }
+
+// remove item
+function handleRemoveContract(contractId) {
+    contracts = contracts.filter(contract => {
+        return contract.id !== contractId;
+    });
+
+}
+
 
 loadContracts();
