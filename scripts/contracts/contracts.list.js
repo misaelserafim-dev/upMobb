764f18eee67baa7da83834afd8d450aeab62dd09
Misael Serafim <misael.serafim95@gmail.com>
2026-05-20 20:17:28 -0300

    add modal preview e correcoes

--- scripts/contracts/contracts.list.js
+++ scripts/contracts/contracts.list.js
@@ -1,3 +1,5 @@
+import { openPreviewModal } from "./contracts.modal.js";
+
 function getStatusLabel(status) {
     const statusMap = {
     1: "Não enviado",
@@ -67,36 +69,30 @@ function createContractRow(contract) {
       </div>
 
       <ul class="contract-list">
-        <li><strong>Criado:</strong> ${createdAt}</li>
         <li><strong>Modelo:</strong> ${model}</li>
+        <li><strong>Criado:</strong> ${createdAt}</li>
         <li><strong>Contratante:</strong> ${contractor}</li>
-        <li><strong>Documento:</strong> ${document}</li>
         <li><strong>Email:</strong> ${email}</li>
       </ul>
+
       <details class="contract-details">
         <summary>Mais detalhes</summary>
           <ul class="contract-list">
             <li><strong>Tipo documento:</strong>${contract.documentType}</li>
+            <li><strong>Documento:</strong> ${document}</li>
             <li><strong>Endereço:</strong> ${address}, ${number}, ${district}, ${city} - ${state}</li>
             <li><strong>CEP:</strong>${contract.zipCode}</li>
           </ul>
       </details>
       
-
-      <div class="contract-column">
-        <div class="contract-actions">
-          <button type="button">
-            Prévia
-          </button>
-        </div>
-      </div>
+      <button type="button" class="preview-button" data-preview="${id}">Prévia</button>
     </div>
   `;
 }
 
+
 function bindRemoveEvents(onRemove) {
   const buttons = document.querySelectorAll(".remove-button");
-
 
   buttons.forEach(button => {
     button.addEventListener("click", () => {
@@ -104,6 +100,24 @@ function bindRemoveEvents(onRemove) {
       
       console.log("id contrato", button.dataset.id);
       onRemove(contractId);
+    });
+  });
+}
+
+function bindPreviewEvents(contracts) {
+  const buttons = document.querySelectorAll(".preview-button");
+
+  buttons.forEach(button => {
+    button.addEventListener("click", () => {
+      const contractId = Number(button.dataset.preview);
+
+      const contract = contracts.find(item => {
+        return item.id === contractId;
+      });
+
+      if (!contract) {  return; }
+
+      openPreviewModal(contract);
     });
   });
 }
@@ -120,4 +134,5 @@ export function renderContracts(container, contracts, onRemove) {
   container.innerHTML = `${rows}`;
 
   bindRemoveEvents(onRemove);
+  bindPreviewEvents(contracts);
 }
---
+++ scripts/contracts/contracts.modal.js
@@ -0,0 +1,68 @@
+function parseParagraph(paragraph) {
+  if (paragraph.startsWith("-")) {
+    return `
+      <li>
+        ${paragraph.replace("-", "").trim()}
+      </li>
+    `;
+  }
+
+  const boldText =
+    paragraph.replace(
+      /\*\*(.*?)\*\*/g,
+      "<strong>$1</strong>"
+    );
+
+    return `
+        <p>${boldText}</p>
+    `;
+}
+
+export function openPreviewModal(contract) {
+    const modal = document.querySelector("#preview-modal");
+    const paragraphs = contract.paragraphs.map(parseParagraph).join("");
+
+    modal.innerHTML = `
+        <div class="preview-overlay">
+            <div class="preview-content">
+                <button class="close-preview">&times;</button>
+
+                <h2>${contract.model}</h2>
+
+                <ul class="contract-list">
+                    <li><strong>Criado:</strong> <span>${contract.createdAt}</span></li>
+                    <li><strong>Contratante:</strong> <span>${contract.contractor}</span></li>
+                    <li><strong>Documento:</strong> <span>${contract.document}</span></li>
+                    <li><strong>Email:</strong> <span>${contract.email}</span></li>
+                    <li><strong>Tipo documento:</strong> <span>${contract.documentType}</span></li>
+                    <li><strong>Endereço:</strong> <span>${contract.address}, ${contract.number}, ${contract.district}, ${contract.city} - ${contract.state}</span></li>
+                    <li><strong>CEP:</strong> <span>${contract.zipCode}</span></li>
+                </ul>
+
+                <div class="preview-text">
+                    <h4>Mais detalhes:</h4>
+                    <div class="preview-information">
+                        ${paragraphs}
+                    </div>
+                </div>
+            </div>
+        </div>
+    `;
+
+    modal.classList.add("active");
+
+    modal.onclick = event => {
+        const closeButton = event.target.closest(".close-preview");
+        const overlay = event.target.closest(".preview-overlay");
+
+        if(closeButton || event.target === overlay) {
+            closePreviewModal();
+        }
+    };
+}
+
+export function closePreviewModal() {
+  const modal = document.querySelector("#preview-modal");
+  modal.classList.remove("active");
+  modal.innerHTML = "";
+}
